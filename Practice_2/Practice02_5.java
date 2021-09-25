a2f33318655be536b86cd17a55e3bd714c4dd022
hhaggang22 <20181018@sungshin.ac.kr>
2021-09-25 22:39:24 +0900

    연습문제 02_5

---
+++ Practice_2/Practice02_5.java
@@ -0,0 +1,14 @@
+package Practice_2;
+
+import java.util.*;
+
+public class Practice02_5{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+
+        String ex = s.nextLine();
+        String upex = ex.toUpperCase();
+
+        System.out.println(upex);
+    }
+}
