551c8b498e313cf2a59fde0b285d34a954cacfb6
hhaggang22 <20181018@sungshin.ac.kr>
2021-09-25 23:09:06 +0900

    연습문제 02_4

--- Practice_2/Practice02_4.java
+++ Practice_2/Practice02_4.java
@@ -1,34 +1,38 @@
 package Practice_2;
 
-import javax.swing.text.html.HTMLDocument.RunElement;
-
 public class Practice02_4 {
-    public static void main(String[] args){
-        //idea : 만약 arr에 있는 값이 result에 없으면 result에 추가한다. 
-        int[] arr = {5, 10, 9, 27, 2, 8, 10, 4, 27, 1};
+    public static void main(String[] args) {
+        // idea : 만약 arr에 있는 값이 result에 없으면 result에 추가한다.
+        int[] arr = { 5, 10, 9, 27, 2, 8, 10, 4, 27, 1 };
         int[] result = new int[10];
+
+        boolean isSame = false;
 
         result[0] = arr[0];
 
-        for(int i = 1; i < arr.length; i++){
-            
-            for(int k = 0; k < result.length; k++){
-                if(arr[i] == result[k]){
-                    result[i] = 0;
-                    break;                  
+        for (int i = 1; i < arr.length; i++) {
+            for (int k = 0; k < result.length; k++) {
+                if (arr[i] == result[k]) {
+                    result[i] = 0; // 만약 arr에서 result 배열로 넣으려는 값이 result에 이미 존재하면 0 넣음
+                    isSame = true; // 만약 이미 result안에 같은 값이 존재하면 플래그를 true
                 }
             }
-            result[i] = arr[i];
+            if (!isSame) {
+                result[i] = arr[i]; // 만약 arr에서 result 배열로 넣으려는 값이 result에 존재하지 않으면 넣음
+            }
+            isSame = false; // 다시 플래그 false 로 초기화
         }
 
+        // 결과
         System.out.println("arr is");
-        for(int j = 0; j < arr.length; j++){
-            System.out.print(arr[j]+",");
+        for (int num : arr) {
+            System.out.print(num + ", ");
         }
+
         System.out.println("\nresult is");
-        for(int j = 0; j < result.length; j++){
-            System.out.print(result[j]+",");
+        for (int data : result) {
+            System.out.print(data + ", ");
         }
     }
-    
+
 }
