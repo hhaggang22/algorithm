c54207e397cb28df4c0e9ebe7027315eb8c10cfc
hhaggang22 <20181018@sungshin.ac.kr>
2021-09-27 23:48:44 +0900

    chap 03. 연결리스트

---
+++ Repeat/SinglelyLinkedList.java
@@ -0,0 +1,15 @@
+package Repeat;
+
+public class SinglelyLinkedList {
+
+    class Node{
+        private int value;
+        private Node next;
+
+        Node(int value, Node next){
+            this.value = value;
+            this.next = next;
+        }
+    }
+    
+}
