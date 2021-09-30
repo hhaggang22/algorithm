5034ba46ce91626b0ffde51a479abf86b2f923ef
hhaggang22 <20181018@sungshin.ac.kr>
2021-09-30 16:42:02 +0900

    chap03. 이중 연결 리스트 삽입

--- chap03/DoubleLinkedList.java
+++ chap03/DoubleLinkedList.java
@@ -34,4 +34,63 @@ public class DoubleLinkedList {
 
     }
 
+    public Node getHead() {
+        return this.head;
+    }
+
+    // 삽입
+    public void append(int value) {
+        // 만약 노드가 아무것도 없으면 삽입하려는 노드가 첫 노드가 됨
+        if (this.head == null) {
+            this.head = new Node(value);
+            return;
+        }
+        // 노드가 있을 때
+        Node pointer = this.head;
+        if (pointer.getValue() != value && pointer.next != null) {
+            pointer = pointer.next;
+        }
+
+        Node newNode = new Node(value);
+        newNode.prev = pointer;
+        pointer.next = newNode;
+
+    }
+
+    public void printPrevNode(int value) {
+        if (this.head == null) {
+            System.out.println("노드가 아무것도 없음");
+            return;
+        }
+
+        if (this.head.getValue() == value) {
+            System.out.println(String.format("%s 이전의 노드가 없음", value));
+            return;
+        }
+
+        Node pointer = this.head;
+        while (pointer.getValue() != value && pointer.next != null) {
+            pointer = pointer.next;
+        }
+
+        if (pointer == null) {
+            System.out.println(String.format("노드 %s 는 존재하지 않음", value));
+        } else {
+            System.out.println(String.format("노드 %s의 이전 노드 값은 %s 임", value, pointer.prev.getValue()));
+        }
+
+    }
+
+    public void printAll() {
+        Node pointer = this.head;
+
+        StringBuilder builder = new StringBuilder();
+        while (pointer != null) {
+            builder.append(pointer.getValue());
+            builder.append("->");
+            pointer = pointer.next;
+        }
+        System.out.println(builder.toString());
+    }
+
 }
