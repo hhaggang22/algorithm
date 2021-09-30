1fc7f6fe8c57934f46a8c3710492bf004cf0d461
hhaggang22 <20181018@sungshin.ac.kr>
2021-09-30 18:31:31 +0900

    chap03. 연습문제 03_1

---
+++ chap03/Practice/Practice03_1.java
@@ -0,0 +1,15 @@
+package chap03.Practice;
+
+public class Practice03_1 {
+    public static void main(String[] args) {
+        SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
+        singlyLinkedList.append(2);
+        singlyLinkedList.append(3);
+        singlyLinkedList.append(4);
+        singlyLinkedList.append(5);
+        singlyLinkedList.append(6);
+        singlyLinkedList.append(7);
+
+        singlyLinkedList.printAll();
+    }
+}
--- chap03/Practice/SinglyLinkedList.java
+++ chap03/Practice/SinglyLinkedList.java
@@ -1,13 +1,13 @@
-package chap03;
+package chap03.Practice;
 
-public class SinglelyLinkedList {
+public class SinglyLinkedList {
     private Node head;
 
-    SinglelyLinkedList() {
+    SinglyLinkedList() {
         this.head = null;
     }
 
-    SinglelyLinkedList(int value) {
+    SinglyLinkedList(int value) {
         this.head = new Node(value, null);
     }
 
@@ -40,37 +40,15 @@ public class SinglelyLinkedList {
 
         // 노드가 이미 존재할 때
         Node pointer = this.head;
+
         while (pointer.next != null) {
+            if (pointer.getValue() == value) {
+                return;
+            }
             pointer = pointer.next;
         }
 
         pointer.next = new Node(value, null);
-    }
-
-    public void delete(int value) {
-        Node pointer = this.head;
-
-        // 맨 앞의 노드를 삭제
-        if (pointer.getValue() == value) {
-            Node removedNode = this.head;
-            this.head = this.head.next;
-
-            removedNode = null;
-            return;
-        }
-
-        Node temp = pointer; // 삭제할 노드 앞의 노드를 가리키는 포인터
-        if (pointer.getValue() != value && pointer.next != null) {
-            temp = pointer;
-            pointer = pointer.next;
-        }
-
-        if (pointer.next == null) { // 끝까지 갔다는 의미
-            temp.next = null;
-        } else {
-            temp.next = pointer.next;
-        }
-        pointer = null;
     }
 
     // 모든 노드를 출력하는 함수
@@ -83,7 +61,7 @@ public class SinglelyLinkedList {
             builder.append("->");
             pointer = pointer.next;
         }
-
+        builder.append(pointer.getValue());
         System.out.println(builder.toString());
     }
 }
