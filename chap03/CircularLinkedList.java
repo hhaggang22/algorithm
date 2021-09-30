b0409b2bcf4e81f78f6bb39599bc562cb31755a5
hhaggang22 <20181018@sungshin.ac.kr>
2021-09-30 17:29:28 +0900

    chap03. 원형단일연결리스트 삭제

--- chap03/CircularLinkedList.java
+++ chap03/CircularLinkedList.java
@@ -4,27 +4,28 @@ public class CircularLinkedList {
     private Node head;
     private Node tail;
 
-    CircularLinkedList(){
+    CircularLinkedList() {
         this.head = null;
         this.tail = null;
     }
 
-    class Node{
-        private int value; 
+    class Node {
+        private int value;
         private Node next;
 
-        Node(int value){
+        Node(int value) {
             this.value = value;
             this.next = null;
         }
 
-        public int getValue(){
+        public int getValue() {
             return this.value;
         }
     }
 
-    public void append(int value){
-        if(this.head == null && this.tail == null){
+    // 삽입
+    public void append(int value) {
+        if (this.head == null && this.tail == null) {
             Node node = new Node(value);
             this.head = node;
             this.tail = node;
@@ -32,11 +33,37 @@ public class CircularLinkedList {
         }
 
         Node pointer = this.tail;
-        
+
         pointer.next = new Node(value);
         this.tail = pointer.next;
         this.tail.next = head;
     }
 
-    
+    // 삭제
+    public void delete(int value) {
+        Node pointer = this.head;
+        // 삭제하려는 노드가 맨 앞의 노드일 때
+        if (pointer.getValue() == value) {
+            Node removeNode = this.head;
+            this.head = this.head.next;
+
+            removeNode = null;
+            return;
+        }
+
+        Node temp = null;
+        while (pointer.getValue() != value && pointer.next != this.tail) {
+            temp = pointer;
+            pointer = pointer.next;
+        }
+
+        if (pointer.next.getValue() == value) {
+            this.tail = pointer;
+            this.tail.next = head;
+        } else {
+            temp.next = pointer.next;
+        }
+        pointer = null;
+    }
+
 }
