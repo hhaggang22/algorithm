e2b4cea9a0eb648f1a2e9d3ecef62407380a262d
hhaggang22 <20181018@sungshin.ac.kr>
2021-10-05 23:06:24 +0900

    chap04. 연습문제 04_2

--- chap04/Practice04_2.java
+++ chap04/Practice04_2.java
@@ -1,5 +1,16 @@
 package chap04;
 
 public class Practice04_2 {
-    
+    public static void main(String[] args) {
+
+    }
+
+    public void loop(int current, int max) {
+
+    }
+
+    public void star(int max) {
+
+    }
+
 }
