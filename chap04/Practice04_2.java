f65ff9109edb4b350c41b1307a6f9e56b798e0d8
hhaggang22 <20181018@sungshin.ac.kr>
2021-10-08 23:35:44 +0900

    chap04. 연습문제 04_2

--- chap04/Practice04_2.java
+++ chap04/Practice04_2.java
@@ -2,15 +2,39 @@ package chap04;
 
 public class Practice04_2 {
     public static void main(String[] args) {
+        print(4);
+    }
+
+    static void blank(int blank) {
+        if (blank == 0) {
+            return;
+        }
+        System.out.print(" ");
+
+        blank(blank - 1);
 
     }
 
-    public void loop(int current, int max) {
+    static void star(int star) {
+        if (star == 0) {
+            return;
+        }
+        System.out.print("* ");
+
+        star(star - 1);
 
     }
 
-    public void star(int max) {
+    static void print(int n) {
+        if (n == 0) {
+            return; // 기저 조건
+        }
 
+        blank(n - 1);
+        star(5 - n);
+        System.out.println("");
+
+        print(n - 1);
     }
 
 }
