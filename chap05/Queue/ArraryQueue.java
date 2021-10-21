1c317495e37f41ae448d45112029f460b8c5b6cf
hhaggang22 <20181018@sungshin.ac.kr>
2021-10-21 23:46:28 +0900

    chap05. 큐

--- chap05/Queue/ArraryQueue.java
+++ chap05/Queue/ArraryQueue.java
@@ -1,9 +1,26 @@
 package chap05.Queue;
 
 public class ArraryQueue {
+    private Object[] arr;
+    private int MAX_QUEUE_SIZE;
+    private int front;
+    private int rear;
+    private int peek;
 
-    ArraryQueue() {
+    ArraryQueue(int size) {
+        this.arr = new Object[size];
+        this.MAX_QUEUE_SIZE = size;
+        this.front = 0;
+        this.rear = -1;
+        this.peek = front;
 
     }
 
+    public void enqueue(Object value) {
+        if (MAX_QUEUE_SIZE - 1 == rear) {
+            throw new StackOverflowError();
+        }
+
+        arr[++rear] = value;
+    }
 }
