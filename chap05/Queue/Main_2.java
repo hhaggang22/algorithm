411ae7b237b76384ac89c21f36395f244be1d8ce
hhaggang22 <20181018@sungshin.ac.kr>
2021-10-26 22:35:01 +0900

    chap05. 연결리스트큐 dequeue 구현

---
+++ chap05/Queue/Main_2.java
@@ -0,0 +1,8 @@
+package chap05.Queue;
+
+public class Main_2 {
+    public static void main(String[] args){
+        Practice05_2_1_LinkedList queue = new Practice05_2_1_LinkedList();
+    }
+    
+}
--- chap05/Queue/Practice05_2_1_LinkedList.java
+++ chap05/Queue/Practice05_2_1_LinkedList.java
@@ -1,6 +1,6 @@
 package chap05.Queue;
 
-public class LinkedListQueue {
+public class Practice05_2_1_LinkedList {
     private Node front = null;
     private Node rear = null;
     private Node peek = null;
@@ -18,15 +18,34 @@ public class LinkedListQueue {
         }
     }
 
-    public void enqueue(Object value){
-        if(front == null){
+    public void enqueue(Object value) {
+        if (front == null) {
             front = new Node(value);
-            peek = front; 
+            peek = front;
             rear = front;
-        }else{
+        } else {
             rear.next = new Node(value);
             rear = rear.next;
         }
+    }
+
+    public Object dequeue() {
+        Object value = peek.getValue();
+        Node temp = front;
+
+        front = front.next;
+        peek = front;
+        temp = null;
+
+        return value;
+    }
+
+    public Object peek() {
+        return peek.getValue();
+    }
+
+    public boolean isEmpty() {
+        return peek == null;
     }
 
 }
