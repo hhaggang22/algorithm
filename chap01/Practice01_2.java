2566f2eca75d81ef4672dc9ee3bc6cd7b590c76a
hhaggang22 <20181018@sungshin.ac.kr>
2021-11-10 22:58:25 +0900

    chap01. 연습문제 01_2

--- chap01/Practice01_2.java
+++ chap01/Practice01_2.java
@@ -6,8 +6,26 @@ public class Practice01_2 {
         System.out.println("최댓값 : " + max);
     }
 
-    public static int maxValue(int data1, int data2, int data3, int data4, int data5){
-        int max = 
+    public static int maxValue(int data1, int data2, int data3, int data4, int data5) {
+        int max = data1;
+
+        if (data2 >= max) {
+            max = data2;
+        }
+
+        if (data3 >= max) {
+            max = data3;
+        }
+
+        if (data4 >= max) {
+            max = data4;
+        }
+
+        if (data5 >= max) {
+            max = data5;
+        }
+
+        return max;
     }
 
 }
