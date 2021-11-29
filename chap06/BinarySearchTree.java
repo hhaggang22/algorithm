16b90c6b2f9250713a5c2ad4c0d6645d23a3375f
hhaggang22 <20181018@sungshin.ac.kr>
2021-11-29 20:28:24 +0900

    chap06. 이진탐색트리 검색

---
+++ chap06/BinarySearchTree.java
@@ -0,0 +1,83 @@
+package chap06;
+
+public class BinarySearchTree {
+    private Node root;
+
+    // 루트 세팅
+    public void setRoot(Node node) {
+        this.root = node;
+    }
+
+    // 루트 가져오기
+    public Node getRoot() {
+        return this.root;
+    }
+
+    // Node 내부 class
+    public class Node {
+        private int value;
+        private Node left = null;
+        private Node right = null;
+
+        Node(int value) {
+            this.value = value;
+        }
+
+        public Node(int value, Node left, Node right) {
+            this.value = value;
+            this.left = left;
+            this.right = right;
+        }
+
+        public int getValue() {
+            return this.value;
+        }
+
+        public void setLeftChild(Node node) {
+            this.left = node;
+        }
+
+        public void setRightChild(Node node) {
+            this.right = node;
+        }
+
+        public Node getLeftChild() {
+            return this.left;
+        }
+
+        public Node getRightChild() {
+            return this.right;
+        }
+    }
+
+    // 재귀 함수를 통한 이진탐색트리 검색
+    public Node search(Node node, int value) {
+        if (node == null) {
+            return null;
+        } else if (node.getValue() == value) {
+            return node;
+        } else if (node.getValue() > value) {
+            return search(node.getLeftChild(), value);
+        } else {
+            return search(node.getRightChild(), value);
+        }
+    }
+
+    // 반복문을 통한 이진탐색트리 검색
+    public Node search(int value) {
+        if (this.root == null) {
+            return null;
+        }
+
+        Node target = this.root;
+        while (target != null && target.getValue() != value) {
+            if (target.getValue() > value) {
+                target = target.getLeftChild();
+            } else {
+                target = target.getRightChild();
+            }
+        }
+        return target;
+    }
+
+}
--- chap06/CompleteBinaryTree.java
+++ chap06/CompleteBinaryTree.java
@@ -1,6 +1,7 @@
 package chap06;
 
 public class CompleteBinaryTree {
+    // 완전 이진트리는 이진트리랑 다르게 인덱스로 노드를 탐색할 수 있다.
     public void printAll(int[] arr) {
         for (int i = 1; i < arr.length; i++) {
             int leftNode = this.getLeftNode(arr, i);
@@ -17,7 +18,7 @@ public class CompleteBinaryTree {
     }
 
     private int getLeftNode(int[] arr, int index) {
-        int findIndex = 2 * index;
+        int findIndex = 2 * index; // 왼쪽 자식 노드 구하는 공식 = 2x인덱스
         if (arr.length <= findIndex) {
             return -1;
         }
@@ -26,7 +27,7 @@ public class CompleteBinaryTree {
     }
 
     private int getRightNode(int[] arr, int index) {
-        int findIndex = (2 * index) + 1;
+        int findIndex = (2 * index) + 1; // 오른쪽 자식 노드 구하는 공식 = (2x인덱스) + 1
         if (arr.length <= findIndex) {
             return -1;
         }
