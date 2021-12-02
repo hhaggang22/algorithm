e7ba1b469393a758300cc815aa0d0552ce1ccae3
hhaggang22 <20181018@sungshin.ac.kr>
2021-12-02 23:30:22 +0900

    chap06. 이진탐색트리 탐색 방법

---
+++ chap06/Traversal.java
@@ -0,0 +1,40 @@
+package chap06;
+
+import chap06.BinarySearchTree.Node;
+
+public class Traversal {
+
+    // 전위순회
+    public void preorder(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        System.out.println(" " + node.getValue());
+        preorder(node.getLeftChild());
+        preorder(node.getRightChild());
+    }
+
+    // 중위순회
+    public void inorder(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        preorder(node.getLeftChild());
+        System.out.println(" " + node.getValue());
+        preorder(node.getRightChild());
+    }
+
+    // 후위순회
+    public void postorder(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        preorder(node.getLeftChild());
+        preorder(node.getRightChild());
+        System.out.println(" " + node.getValue());
+    }
+
+}
